b43b7da524b53131eccf44aaf401420d3af10da3
Baxter Morley <baxter@reyvard.tk>
2013-07-13 21:07:03 +0100

    Stuff for my Bootstrap based site

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+ 
+ <html>
+ <head>
+ <link rel="icon" type="image/ico" href="favicon.ico" />
+ 
+			<meta charset="utf-8">
+			<meta property="og:image" content="http://alternativelink.tk/nextbigthing/favicon.png"/>
+			<title>Awesome Beta Stuff!</title>
+			<link rel="stylesheet" href="http://flip.hr/css/bootstrap.min.css"
+</head>
+ <body>
+ 
+ <div class="container">
+ 
+		 <div class="hero-unit">
+		 <h2>OMG! Boxes everywhere! (cause Boxes are sexy!)</h2>
+		 <p>Hey peeps! I am ".hero-unit" (sounds like a TV program!) I am just a huge box for important words or media</p>
+		 <P><a class="btn btn-primary btn-large">Superman ain't got nuttin me! &raquo;</a></p>
+		 </div><!-- .hero-unit -->
+		 
+<div class="row">
+		<div class="span4">
+		<h2>I'm a box (Not a Giraffe!)</h2>
+		<p>I wish I was a Giraffe Though</p>
+		<p><a class="btn" href="#">Just wating for Baxter to give me a function! &raquo;</a></p>
+		</div><!-- .span4 -->
+		
+		<div class="row">
+		<div class="span4">
+		<h2>I'm a box (Not a Rhino)</h2>
+		<p>The only thing sharper than my horn is this layout!</p>
+		<p><a class="btn" href="#">Baxter, Where are you? &raquo;</a></p>
+		</div><!-- .span4 -->
+		
+		<div class="row">
+		<div class="span4">
+		<h2>RAWR I'm T-Rex!</h2>
+		<p>No, wait I'm a Box. Yup, deffo a box!</p>
+		<p><a class="btn" href="#">GET YOUR ARSE IN THE CODE EDITOR NOW! &raquo;</a></p>
+		</div><!-- .span4 -->
+		
+		</div><!-- .row -->
+ </div><!-- .container -->
+     
+ 
+ <div id="this-carousel-id" class="carousel slide"><!-- class of slide for animation -->
+  <div class="carousel-inner">
+    <div class="item active"><!-- class of active since it's the first item -->
+      <img src="banner stuff/banner 1.png" alt="" />
+      <div class="carousel-caption">
+        <p>Blue Wave</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="banner stuff/banner 2.png" alt="" />
+      <div class="carousel-caption">
+        <p>Orange Wave</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="banner stuff/banner 3.png" alt="" />
+      <div class="carousel-caption">
+        <p>Green Wave</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="banner stuff/banner 4.png" alt="" />
+      <div class="carousel-caption">
+        <p>Purple Wave</p>
+      </div>
+    </div>
+  </div><!-- /.carousel-inner -->
+  <!--  Next and Previous controls below
+        href values must reference the id for this carousel -->
+    <a class="carousel-control left" href="#this-carousel-id" data-slide="prev">&lsaquo;</a>
+    <a class="carousel-control right" href="#this-carousel-id" data-slide="next">&rsaquo;</a>
+</div><!-- /.carousel -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
+<script src="js/bootstrap.min.js"></script>
+ </body>
+</html>
